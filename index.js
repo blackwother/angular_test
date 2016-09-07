3d8282949de95c47a614bfcf9783fbaaf7f0325d
blackwother <n.blackwother@gmail.com>
2016-09-07 22:32:56 +0300

    create basic routes

---
+++ index.js
@@ -0,0 +1,26 @@
+{
+  "name": "medx",
+  "version": "1.1.1",
+  "description": "example",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/alexwander/EDX_mean.git"
+  },
+  "author": "alexwander",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/alexwander/EDX_mean/issues"
+  },
+  "homepage": "https://github.com/alexwander/EDX_mean#readme",
+  "dependencies": {
+    "gulp": "~3.9.1",
+    "gulp-mocha": "~3.0.1",
+    "mongodb": "~2.2",
+    "mongoose": "~4.0.3",
+    "underscore": "~1.5.2"
+  }
+}
